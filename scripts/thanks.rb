ecb8fb8e3b173e109591af949a796aeb41d7adff
Richard Lister <rlister+gh@gmail.com>
2015-03-08 17:36:22 -0400

    more politeness

---
+++ scripts/thanks.rb
@@ -0,0 +1,13 @@
+## be polite botsy
+hear /\btha?n?(x|ks|k\s*you)\W+botsy\b/i do
+  [
+    "you're welcome",
+    "no worries",
+    "don't mention it",
+    "any time",
+    "sure thing, #{user_name}",
+    "it was nothing",
+    "by all means",
+    "my pleasure"
+  ].sample
+end
