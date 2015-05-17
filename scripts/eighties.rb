4314a0f5c7d2b8f9d1b614a6e5a78eaa936ec5a4
Richard Lister <rlister+gh@gmail.com>
2015-05-16 21:25:42 -0400

    talk like a valley girl

---
+++ scripts/eighties.rb
@@ -0,0 +1,3 @@
+hear /\b(z?omg|soo+|80'?s|eighties|198[0-9]|to the max|gag|grody|mall)\b/i do
+  [ 'like, totally', 'gag me with a spoon', 'bitchin\'', 'totally tubular', 'grody to the max', 'I\'m sooo sure', 'barf' ].sample
+end
